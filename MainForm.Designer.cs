dde95cc1474bc337fb6e2abc23a59610d51a5684
Jared Kells <jkells@gmail.com>
2016-09-17 13:22:01 +1000

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace AreTheseFoldersInDropbox
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.goBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dropboxFolderTxt = new System.Windows.Forms.TextBox();
+            this.folderTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowseBtn = new System.Windows.Forms.Button();
+            this.dropboxFolderBrowseBtn = new System.Windows.Forms.Button();
+            this.resultsTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderRecurse = new System.Windows.Forms.CheckBox();
+            this.dropboxFolderRecurse = new System.Windows.Forms.CheckBox();
+            this.searchPattern = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.copyMissing = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // goBtn
+            // 
+            this.goBtn.Location = new System.Drawing.Point(369, 347);
+            this.goBtn.Name = "goBtn";
+            this.goBtn.Size = new System.Drawing.Size(75, 23);
+            this.goBtn.TabIndex = 0;
+            this.goBtn.Text = "Go";
+            this.goBtn.UseVisualStyleBackColor = true;
+            this.goBtn.Click += new System.EventHandler(this.goBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Folder";
+            // 
+            // dropboxFolderTxt
+            // 
+            this.dropboxFolderTxt.Location = new System.Drawing.Point(98, 67);
+            this.dropboxFolderTxt.Name = "dropboxFolderTxt";
+            this.dropboxFolderTxt.Size = new System.Drawing.Size(265, 20);
+            this.dropboxFolderTxt.TabIndex = 2;
+            // 
+            // folderTxt
+            // 
+            this.folderTxt.Location = new System.Drawing.Point(98, 17);
+            this.folderTxt.Name = "folderTxt";
+            this.folderTxt.Size = new System.Drawing.Size(265, 20);
+            this.folderTxt.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "DropBox Folder";
+            // 
+            // folderBrowseBtn
+            // 
+            this.folderBrowseBtn.Location = new System.Drawing.Point(369, 15);
+            this.folderBrowseBtn.Name = "folderBrowseBtn";
+            this.folderBrowseBtn.Size = new System.Drawing.Size(75, 23);
+            this.folderBrowseBtn.TabIndex = 5;
+            this.folderBrowseBtn.Text = "Browse";
+            this.folderBrowseBtn.UseVisualStyleBackColor = true;
+            this.folderBrowseBtn.Click += new System.EventHandler(this.folderBrowseBtn_Click);
+            // 
+            // dropboxFolderBrowseBtn
+            // 
+            this.dropboxFolderBrowseBtn.Location = new System.Drawing.Point(369, 65);
+            this.dropboxFolderBrowseBtn.Name = "dropboxFolderBrowseBtn";
+            this.dropboxFolderBrowseBtn.Size = new System.Drawing.Size(75, 23);
+            this.dropboxFolderBrowseBtn.TabIndex = 6;
+            this.dropboxFolderBrowseBtn.Text = "Browse";
+            this.dropboxFolderBrowseBtn.UseVisualStyleBackColor = true;
+            this.dropboxFolderBrowseBtn.Click += new System.EventHandler(this.dropboxFolderBrowseBtn_Click);
+            // 
+            // resultsTxt
+            // 
+            this.resultsTxt.Location = new System.Drawing.Point(15, 191);
+            this.resultsTxt.Multiline = true;
+            this.resultsTxt.Name = "resultsTxt";
+            this.resultsTxt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultsTxt.Size = new System.Drawing.Size(429, 150);
+            this.resultsTxt.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Results";
+            // 
+            // folderRecurse
+            // 
+            this.folderRecurse.AutoSize = true;
+            this.folderRecurse.Location = new System.Drawing.Point(98, 43);
+            this.folderRecurse.Name = "folderRecurse";
+            this.folderRecurse.Size = new System.Drawing.Size(66, 17);
+            this.folderRecurse.TabIndex = 9;
+            this.folderRecurse.Text = "Recurse";
+            this.folderRecurse.UseVisualStyleBackColor = true;
+            // 
+            // dropboxFolderRecurse
+            // 
+            this.dropboxFolderRecurse.AutoSize = true;
+            this.dropboxFolderRecurse.Location = new System.Drawing.Point(98, 93);
+            this.dropboxFolderRecurse.Name = "dropboxFolderRecurse";
+            this.dropboxFolderRecurse.Size = new System.Drawing.Size(66, 17);
+            this.dropboxFolderRecurse.TabIndex = 10;
+            this.dropboxFolderRecurse.Text = "Recurse";
+            this.dropboxFolderRecurse.UseVisualStyleBackColor = true;
+            // 
+            // searchPattern
+            // 
+            this.searchPattern.Location = new System.Drawing.Point(98, 116);
+            this.searchPattern.Name = "searchPattern";
+            this.searchPattern.Size = new System.Drawing.Size(119, 20);
+            this.searchPattern.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Search Pattern";
+            // 
+            // copyMissing
+            // 
+            this.copyMissing.AutoSize = true;
+            this.copyMissing.Location = new System.Drawing.Point(98, 143);
+            this.copyMissing.Name = "copyMissing";
+            this.copyMissing.Size = new System.Drawing.Size(114, 17);
+            this.copyMissing.TabIndex = 13;
+            this.copyMissing.Text = "Copy missing files?";
+            this.copyMissing.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 382);
+            this.Controls.Add(this.copyMissing);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.searchPattern);
+            this.Controls.Add(this.dropboxFolderRecurse);
+            this.Controls.Add(this.folderRecurse);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultsTxt);
+            this.Controls.Add(this.dropboxFolderBrowseBtn);
+            this.Controls.Add(this.folderBrowseBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.folderTxt);
+            this.Controls.Add(this.dropboxFolderTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.goBtn);
+            this.Name = "MainForm";
+            this.Text = "Are all these files in Dropbox already?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button goBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox dropboxFolderTxt;
+        private System.Windows.Forms.TextBox folderTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button folderBrowseBtn;
+        private System.Windows.Forms.Button dropboxFolderBrowseBtn;
+        private System.Windows.Forms.TextBox resultsTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox folderRecurse;
+        private System.Windows.Forms.CheckBox dropboxFolderRecurse;
+        private System.Windows.Forms.TextBox searchPattern;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox copyMissing;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace AreTheseFoldersInDropbox
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Restore();
+
+            Closing += (sender, args) => Save();
+        }
+
+        private void dropboxFolderBrowseBtn_Click(object sender, EventArgs e)
+        {
+            var dlg = new FolderBrowserDialog();
+            dlg.ShowDialog();
+            dropboxFolderTxt.Text = dlg.SelectedPath;
+        }
+
+        private void folderBrowseBtn_Click(object sender, EventArgs e)
+        {
+            var dlg = new FolderBrowserDialog();
+            dlg.ShowDialog();
+            folderTxt.Text = dlg.SelectedPath;
+        }
+
+        private void Restore()
+        {
+            var softwareKey = Registry.CurrentUser.OpenSubKey("Software", true);
+            var appNameKey = softwareKey?.CreateSubKey("AreTheseFoldersInDropbox");
+
+            folderTxt.Text = appNameKey?.GetValue("folder", "") as string ?? "";
+            dropboxFolderTxt.Text = appNameKey?.GetValue("dropboxFolder", "") as string ?? "";
+            searchPattern.Text = appNameKey?.GetValue("searchPattern", "*") as string ?? "*";
+           
+            bool folderRecurseSetting;
+            if (bool.TryParse(appNameKey?.GetValue("folderRecurse", false) as string ?? "", out folderRecurseSetting))
+                folderRecurse.Checked = folderRecurseSetting;
+
+            bool dropboxFolderRecurseSetting;
+            if (bool.TryParse(appNameKey?.GetValue("dropboxFolderRecurse", false) as string ?? "", out dropboxFolderRecurseSetting))
+                dropboxFolderRecurse.Checked = dropboxFolderRecurseSetting;
+
+            bool copyMissingSetting;
+            if (bool.TryParse(appNameKey?.GetValue("copyMissing", false) as string ?? "", out copyMissingSetting))
+                copyMissing.Checked = copyMissingSetting;
+        }
+
+        private void Save()
+        {
+            var softwareKey = Registry.CurrentUser.OpenSubKey("Software", true);
+            var appNameKey = softwareKey?.CreateSubKey("AreTheseFoldersInDropbox");
+
+            appNameKey?.SetValue("folder", folderTxt.Text);
+            appNameKey?.SetValue("folderRecurse", folderRecurse.Checked);
+            appNameKey?.SetValue("dropboxFolder", dropboxFolderTxt.Text);
+            appNameKey?.SetValue("dropboxFolderRecurse", dropboxFolderRecurse.Checked);
+            appNameKey?.SetValue("searchPattern", searchPattern.Text);
+            appNameKey?.SetValue("copyMissing", copyMissing.Checked);
+        }
+
+        private async void goBtn_Click(object sender, EventArgs e)
+        {
+            goBtn.Enabled = false;
+            resultsTxt.Text = "";
+            try
+            {
+                var dropBoxFiles = await ReadDropBoxFiles();
+                var missingFiles = await LookForMissingFiles(dropBoxFiles);
+
+                resultsTxt.Text += $"{missingFiles.Count} files missing\r\n";
+                foreach (var missingFile in missingFiles)
+                {
+                    resultsTxt.Text += $"File missing: {missingFile.FullName}\r\n";
+                }
+
+                if (copyMissing.Checked)
+                {
+                    await CopyMissingFiles(missingFiles);
+                }
+            }
+            catch (Exception exception)
+            {
+                resultsTxt.Text += $"Exception: {exception}";
+            }
+            goBtn.Enabled = true;
+        }
+
+        private async Task CopyMissingFiles(List<FileInfo> missingFiles)
+        {
+            var destination = dropboxFolderTxt.Text;
+            foreach (var missingFile in missingFiles)
+            {
+                resultsTxt.Text += $"Copying: {missingFile.FullName}\r\n";
+                await Task.Run(()=>missingFile.CopyTo($"{destination}{Path.DirectorySeparatorChar}{missingFile.Name}", false));
+            }
+        }
+
+        private async Task<List<FileInfo>> LookForMissingFiles(Dictionary<string, FileInfo> dropBoxFiles)
+        {
+            var searchOptions = folderRecurse.Checked
+                ? SearchOption.AllDirectories
+                : SearchOption.TopDirectoryOnly;
+
+            resultsTxt.Text += "Reading new files\r\n";
+            var files2 = await Task.Run(() => new DirectoryInfo(folderTxt.Text).GetFiles(searchPattern.Text,
+                searchOptions));
+
+            resultsTxt.Text += "Hashing and comparing new files\r\n";
+            var missingFiles = new List<FileInfo>();
+            foreach (var fileInfo in files2)
+            {
+                try
+                {
+                    if (!dropBoxFiles.ContainsKey(await CaclculateMd5(fileInfo)))
+                        missingFiles.Add(fileInfo);
+                }
+                catch (Exception exception)
+                {
+                    resultsTxt.Text += $"Exception processing {fileInfo.FullName}: {exception}\r\n";
+                }
+            }
+            return missingFiles;
+        }
+
+        private async Task<Dictionary<string, FileInfo>> ReadDropBoxFiles()
+        {
+            var searchOptions = dropboxFolderRecurse.Checked
+                ? SearchOption.AllDirectories
+                : SearchOption.TopDirectoryOnly;
+
+            resultsTxt.Text += "Reading DropBox files\r\n";
+            var files1 = await Task.Run(() => new DirectoryInfo(dropboxFolderTxt.Text).GetFiles(searchPattern.Text,
+                searchOptions));
+
+            resultsTxt.Text += $"Hashing {files1.Length} files\r\n";
+
+            var dropBoxFiles = new Dictionary<string, FileInfo>();
+            foreach (var fileInfo in files1)
+            {
+                try
+                {
+                    dropBoxFiles.Add(await CaclculateMd5(fileInfo), fileInfo);
+                }
+                catch (Exception exception)
+                {
+                    resultsTxt.Text += $"Exception copying {fileInfo.FullName}: {exception}\r\n";
+                }
+            }
+            return dropBoxFiles;
+        }
+
+        private Task<string> CaclculateMd5(FileInfo file)
+        {
+            return Task.Run(() =>
+            {
+                using (var md5 = MD5.Create())
+                {
+                    using (var stream = File.OpenRead(file.FullName))
+                    {
+                        return BitConverter.ToString(md5.ComputeHash(stream));
+                    }
+                }
+            });
+        }
+    }
+}
